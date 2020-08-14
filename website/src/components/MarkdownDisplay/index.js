c3232a6feeb200dc7bbfa55aaf3b204c55054b85
Erald Totraku <erald.totraku@softup.co>
2020-08-14 08:17:15 +0200

    support for components inside numbered paragraphs, e.g. markdown

--- website/src/components/MarkdownDisplay/index.js
+++ website/src/components/MarkdownDisplay/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactMarkdown from "react-markdown"
 import PropTypes from "prop-types"
-import './markdown-display.scss'
+import "./markdown-display.scss"
 
 export const MarkdownDisplay = ({ data }) => <ReactMarkdown source={data} />
 
