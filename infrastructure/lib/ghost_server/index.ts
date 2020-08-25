ac96059af1171c4399bd8259a6635e40886922a5
Stivi Ndoni <stivi.ndoni@gmail.com>
2020-08-25 14:39:14 +0200

    Test https only

--- infrastructure/lib/ghost_server/index.ts
+++ infrastructure/lib/ghost_server/index.ts
@@ -99,7 +99,7 @@ export class GhostServerResources extends cdk.Construct {
           ],
           customOriginSource: {
             domainName: this.eip.domain || this.ghostServerInstance.instancePrivateDnsName,
-            originProtocolPolicy: OriginProtocolPolicy.HTTP_ONLY,
+            originProtocolPolicy: OriginProtocolPolicy.HTTPS_ONLY,
           },
         }
       ],
