ce0ed587f42ed2150d63cfb4d1a09ef38d9089f5
Regi Shehi <shehi.regi@gmail.com>
2020-09-01 10:00:39 +0200

    Align vertical lines

--- website/src/components/PortfolioTools/QuickFacts/index.js
+++ website/src/components/PortfolioTools/QuickFacts/index.js
@@ -4,14 +4,14 @@ import PropTypes from "prop-types";
 
 export const QuickFacts = ({ data }) => {
   return (
-    <div className={"displayWithGrid"}>
-      <div className={"quickFactsContainer"}>
+    <div className="displayWithGrid">
+      <div className="quickFactsContainer">
         {data.map((item, index) => (
-          <div key={index} className={"itemContainer"}>
-            <div className={"title"}>{item.title}</div>
-            <div className={"content"}>
+          <div key={index} className="itemContainer">
+            <div className="title">{item.title}</div>
+            <div className="content">
               {item.type === "fact" ? (
-                <div className={"description"}>{item.description}</div>
+                <div className="description">{item.description}</div>
               ) : (
                 item.items.map((word, index) => (
                   <div className={"list"} key={index}>
