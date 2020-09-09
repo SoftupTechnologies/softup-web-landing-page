146502199224c10a244ce75f05592ee8f3305dd1
Jurgen xhaja <jurgen.xhaja@softup.co>
2020-09-09 16:17:44 +0200

    Added career screen

--- website/src/components/PortfolioTools/QuickDescription/index.js
+++ website/src/components/PortfolioTools/QuickDescription/index.js
@@ -5,15 +5,16 @@ import { generateContent } from "../../helpers";
 
 export const QuickDescription = ({ data }) => {
   return (
-    <div className={"quickDescriptionDiv displayWithGrid"}>
-      <div className={"title"}>{data.title}</div>
-      <div className={"description"}>{generateContent(data.content)}</div>
+    <div className={data.no_padding ?"quickDescriptionDiv displayWithGrid no-padding" :"quickDescriptionDiv displayWithGrid"}>
+      {data.title && <div className={"title"}>{data.title}</div>}
+      <div className={data.no_padding ? "new-description" : "description" }>{generateContent(data.content)}</div>
     </div>
   );
 };
 
 QuickDescription.propTypes = {
   data: PropTypes.shape({
+    no_padding: PropTypes.bool,
     title: PropTypes.string,
     content: PropTypes.arrayOf(PropTypes.object),
   }),
