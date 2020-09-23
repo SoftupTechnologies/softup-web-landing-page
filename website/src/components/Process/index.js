6b1900051732e67bcf49e66247484f83f119c0c3
Erald Totraku <erald.totraku@gmail.com>
2020-09-23 16:53:30 +0200

    adjusted process component / content

--- website/src/components/Process/index.js
+++ website/src/components/Process/index.js
@@ -2,36 +2,31 @@ import { useTranslation } from "react-i18next";
 import React from "react";
 import PropTypes from "prop-types";
 import "./process.scss";
+import {MarkdownDisplay} from "../MarkdownDisplay";
 
 export const Process = ({ data }) => {
   const { t } = useTranslation();
-  const items = data?.items.length - 1;
+  const items = data?.items?.length - 1;
 
   return (
     <div>
-      <hr className="process-hr" />
       <div className="displayWithGrid">
         <div className="process-wrapper">
-          <h1 className="process-title">{t(data.title)}</h1>
-          {data?.items.map((item, index) => (
+          {data?.items?.map((item, index) => (
             <div className="process" key={index}>
-              {index !== items && <div className="process-center-line" />}
               <div className="process-article">
-                <div className="process-article-title right">
-                  <p>{t(item.title)}</p>
-                </div>
-                <div className="process-article-description">
-                  <p>{t(item.description)}</p>
-                </div>
                 <div className="outer-circle">
                   <div className="inner-circle">
                     <div className="second-inner-circle">
                       <div className="third-inner-circle">
-                        <span className="process-number">{index + 1}</span>
+                        <span className="process-number">{t(item.title)}</span>
                       </div>
                     </div>
                   </div>
                 </div>
+                  <div className="process-article-description">
+                      <MarkdownDisplay data={t(item.description)} />
+                  </div>
               </div>
             </div>
           ))}
