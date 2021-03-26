9591f2c7720c055b180c969fa5ea470f57c6025a
en3a <enea.dede@elitestudio.io>
2021-03-26 12:13:21 +0100

    add buttons to home screen

--- website/src/components/Landing/components.js
+++ website/src/components/Landing/components.js
@@ -7,15 +7,29 @@ import PropTypes from "prop-types";
 // import { LanguageMenu } from "../LangSwitch"
 import { Link } from "../../../.cache/gatsby-browser-entry";
 import SoftupLogoSvg from "../../images/softup-logo.svg";
+import {Image} from "../image";
+import { Button } from "../Button";
 
 export const Slogan = ({ slogan, description }) => {
   return (
-    <div className="slogan">
-      <h1 className="sloganTitle">
-        {slogan}
-        <div className="diamond-punctuation"></div>
-      </h1>
-      <div className="sloganDesc">{description}</div>
+    <div>
+        <div className="slogan">
+            <h1 className="sloganTitle">
+                {slogan}
+                <div className="diamond-punctuation"></div>
+            </h1>
+            <div className="sloganDesc">{description}</div>
+        </div>
+        <div id="company-numbers" className="companyNumContainer">
+            <div className="companyNumbers">
+                <div className="benchmarkGroup">
+                    <Button data={{openInNewPage:false, link:"mailto:info@softup.co", value:"Reach out to us"}}/>
+                </div>
+                <div className="benchmarkGroup">
+                    <Button data={{openInNewPage:false, link:"/estimator", value:"Estimate your project"}}/>
+                </div>
+            </div>
+        </div>
     </div>
   );
 };
